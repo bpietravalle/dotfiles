eff0848865ff1c13fdd8aa046741a02d16546953
Brian Pietravalle <bpietravalle@gmail.com>
2015-12-02 20:37:53 -0800

    initial

--- .zsh/bindkeys.zsh
+++ .zsh/bindkeys.zsh
@@ -1,21 +1,39 @@
 # To see the key combo you want to use just do:
 # cat > /dev/null
 # And press it
+bindkey -v
 
-bindkey "^K"      kill-whole-line                      # ctrl-k
-bindkey "^R"      history-incremental-search-backward  # ctrl-r
-bindkey "^A"      beginning-of-line                    # ctrl-a  
-bindkey "^E"      end-of-line                          # ctrl-e
-bindkey "[B"      history-search-forward               # down arrow
-bindkey "[A"      history-search-backward              # up arrow
-bindkey "^D"      delete-char                          # ctrl-d
-bindkey "^F"      forward-char                         # ctrl-f
-bindkey "^B"      backward-char                        # ctrl-b
+# bindkey "^K"      kill-whole-line                      # ctrl-k
+# bindkey "^R"      history-incremental-search-backward  # ctrl-r
+# bindkey "^A"      beginning-of-line                    # ctrl-a  
+# bindkey "^E"      end-of-line                          # ctrl-e
+# bindkey "[B"      history-search-forward               # down arrow
+# bindkey "[A"      history-search-backward              # up arrow
+# bindkey "^D"      delete-char                          # ctrl-d
+# bindkey "^F"      forward-char                         # ctrl-f
+# bindkey "^B"      backward-char                        # ctrl-b
 
 bindkey -a 'gg' beginning-of-buffer-or-history
 bindkey -a 'g~' vi-oper-swap-case
 bindkey -a G end-of-buffer-or-history
 bindkey -a u undo
 bindkey -a '^R' redo
+# bindkey '^?' backward-delete-char
+# bindkey '^H' backward-delete-char
+
+bindkey '^P' up-history
+bindkey '^N' down-history
 bindkey '^?' backward-delete-char
-bindkey '^H' backward-delete-char
+bindkey '^h' backward-delete-char
+bindkey '^w' backward-kill-word
+bindkey '^r' history-incremental-search-backward
+
+# function zle-line-init zle-keymap-select {
+#     VIM_PROMPT="%{$fg_bold[yellow]%} [% NORMAL]%  %{$reset_color%}"
+#     RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/}$(git_custom_status) $EPS1"
+#     zle reset-prompt
+# }
+
+# zle -N zle-line-init
+# zle -N zle-keymap-select
+# export KEYTIMEOUT=1
