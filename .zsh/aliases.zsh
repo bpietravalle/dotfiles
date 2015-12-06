5b90a10eeb9a142ab94ac797d1b7b4ec80bf521c
Brian Pietravalle <bpietravalle@gmail.com>
2015-12-06 05:38:10 -0800

    added aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -10,3 +10,15 @@ alias bower='noglob bower'
 
 # Launch chrome with remote debugging port on
 # alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222'
+#
+alias gs='git status '
+alias ga='git add '
+alias gb='git branch '
+alias gc='git commit'
+alias gd='git diff'
+alias go='git checkout '
+alias gk='gitk --all&'
+alias gx='gitx --all'
+
+alias got='git '
+alias get='git '
